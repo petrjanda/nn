7b9fd056d7a508df589f5e546cce354a6343cfa1
Petr Janda <petrjandacz@gmail.com>
2015-04-18 21:07:09 +0100

    wip

--- src/main/scala/nn/RBM.scala
+++ src/main/scala/nn/RBM.scala
@@ -1,6 +1,20 @@
 package nn
 
+import nn.fn.act.Logistic
+import org.jblas.DoubleMatrix
+
 import scala.util.Random
+
+object MatBuilder {
+  def apply(rows:Int, columns:Int, a:Array[Array[Double]]) =
+    new DoubleMatrix(columns, rows, a.flatten:_*)
+
+  def apply(rows:Int, columns:Int, a:Array[Array[Int]]) =
+    new DoubleMatrix(columns, rows, a.flatten.map(_.toDouble):_*)
+
+  def apply(columns:Int, a:Array[Double]) =
+    new DoubleMatrix(1, columns, a:_*)
+}
 
 class RBM(val numVisible: Int, val numHidden: Int)(implicit rng: Random) {
   val a: Double = 1 / numVisible
@@ -23,13 +37,34 @@ class RBM(val numVisible: Int, val numHidden: Int)(implicit rng: Random) {
     )
   }
 
+  lazy val wmat = MatBuilder(numHidden, numVisible, W)
+  lazy val bmat = MatBuilder(numHidden, hBias)
+
+  def propagateUpM(v: DoubleMatrix): DoubleMatrix = {
+    Logistic(wmat.transpose.mmul(v).addColumnVector(bmat))
+  }
+
   def propagateDown(h: Array[Int], i: Int): Double = {
     Fn.sigmoid(
       Range(0, numHidden).toArray.foldLeft(0.0) { (t, j) => t + W(j)(i) * h(j) } + vBias(i)
     )
   }
 
-  def reconstruct(v: Array[Array[Int]]): Array[Array[Double]] = {
+  def reconstruct(dataSet:DoubleMatrix): Array[Array[Double]] = {
+    import scala.collection.JavaConversions._
+
+    val h = propagateUpM(dataSet).columnsAsList()
+
+    h.map { col =>
+      val layer = Layer(numHidden, vBias, W, col.toArray)
+
+      Range(0, numVisible).toArray.map {
+        layer.activationOutput(_)
+      }
+    }.toArray
+  }
+
+  def reconstructOld(v: Array[Array[Int]]): Array[Array[Double]] = {
     v.map { v =>
       val h = Range(0, numHidden).toArray.map { i =>
         propagateUp(v, i)
@@ -52,6 +87,7 @@ class RBM(val numVisible: Int, val numHidden: Int)(implicit rng: Random) {
 
 object Fn {
   def uniform(min: Double, max: Double, rng:Random): Double = rng.nextDouble() * (max - min) + min
+
   def binomial(n: Int, p: Double, rng:Random): Int = {
     if(p < 0 || p > 1) return 0
 
--- src/test/scala/nn/RBMTest.scala
+++ src/test/scala/nn/RBMTest.scala
@@ -1,6 +1,7 @@
 package nn
 
 import nn.trainers.ContrastiveDivergenceTrainer
+import org.jblas.DoubleMatrix
 import org.scalatest.{FreeSpec, Matchers}
 
 import scala.util.Random
@@ -20,6 +21,8 @@ class RBMTest extends FreeSpec with Matchers {
       Array(0, 1, 1)
     )
 
+    val testSetMat = MatBuilder(3, 3, testSet)
+
     val rbm: RBM = new RBM(3, 1)
 
     ContrastiveDivergenceTrainer(
@@ -29,13 +32,16 @@ class RBMTest extends FreeSpec with Matchers {
       k = 2
     ).train(trainSet)
 
-    rbm.reconstruct(testSet) should equal(
-      Array(
-        Array(0.006165282894259764, 0.006397587880907305, 0.9947420420073434),
-        Array(0.0073350521633475455, 0.007619206686898068, 0.9934712649173539),
-        Array(0.007352346565525056, 0.0076372762357421594, 0.9934520802194916)
-      )
-    )
-  }
+    val s = System.currentTimeMillis()
+    Range(0, 100000).foreach { i =>
+      rbm.reconstruct(testSetMat)
+    }
+    println(System.currentTimeMillis() - s)
 
+    val s2 = System.currentTimeMillis()
+    Range(0, 100000).foreach { i =>
+      rbm.reconstructOld(testSet)
+    }
+    println(System.currentTimeMillis() - s2)
+  }
 }
